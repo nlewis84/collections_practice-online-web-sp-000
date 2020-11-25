ab1aafb656da6d586da228d62bf9848219ae4c47
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 16:03:37 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array [2], array[1]
+  array
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array [destination_index], array[index]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |index|
+    index[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |index|
+    index[0] == "a"
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, n|
+    sum += n
+  end
+end
+
+def add_s(array)
+  x = 0
+  array.each do |index|
+    if x != 1
+      index << "s"
+    end
+    x += 1
+  end
+end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -29,7 +29,14 @@ describe 'collections practice' do
       expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
     end
   end
-
+  
+  # Question 4.5
+  describe '#swap_elements_from_to' do
+    it 'specify when elements to swap' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"], 0, 2)).to eq(["scott", "ashley", "blake"])
+    end
+  end
+  
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
